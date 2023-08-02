50c9e1af8aad5f8c2dc382ed1b1c6a6c233ddf18
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-08-02 13:43:25 +0000

    test: improve cases

--- __tests__/abort-controller.spec.ts
+++ __tests__/abort-controller.spec.ts
@@ -1,4 +1,5 @@
 import { AbortController } from '@src/abort-controller.js'
+import { AbortError } from '@src/abort-error.js'
 
 describe('AbortController', () => {
   describe('abort', () => {
@@ -12,18 +13,21 @@ describe('AbortController', () => {
 
       expect(aborted1).toBe(false)
       expect(aborted2).toBe(true)
+      expect(signal.reason).toBeInstanceOf(AbortError)
     })
 
     test('with reason', () => {
       const controller = new AbortController()
       const signal = controller.signal
+      const reason = 'reason'
 
       const aborted1 = signal.aborted
-      controller.abort('reason')
+      controller.abort(reason)
       const aborted2 = signal.aborted
 
       expect(aborted1).toBe(false)
       expect(aborted2).toBe(true)
+      expect(signal.reason).toBe(reason)
     })
   })
 })
