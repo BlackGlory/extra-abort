b80356e3d77da05fc854afc708a8b41cfb75904e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-12-02 04:33:43 +0000

    feat: improve `AbortError`

--- src/abort-error.ts
+++ src/abort-error.ts
@@ -9,9 +9,11 @@ export class AbortError extends CustomError {
           || (
                names.includes('DOMException') &&
 
-               // 不同的实现有不同的错误信息, 收集并检查每一种错误信息的做法效率太低.
-               // 目前可以确定的是每种错误信息都会包含`aborted`字样, 在此将其作为判断的依据.
-               instance.message.includes('aborted')
+               // 不同的实现有不同的错误信息, 但基本上都会包含`aborted`或`cancelled`字样.
+               (
+                instance.message.includes('aborted') ||
+                instance.message.includes('cancelled')
+               )
              )
     }
 
